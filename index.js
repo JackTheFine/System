e1182e3bff68fee4ec74c525a52b197022506af9
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2023-10-21 23:25:49 -0400

    uploaded 2049 on port 8081

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const db = require("./db.js");
 db.loadFromFile("./db.json");
 const app = require("express")();
+const app1 = require("express")();
 var express = require("express")
+var express1 = require("express")
 const path = require("path")
 var port = "8080"
+var port1 = "8081"
 
 app.set("json spaces", 4);
 
 app.use(express.static("public", { extensions: ['html'] }));
+app1.use(express.static("2048", { extensions: ['html'] }));
 
 require("./bots/mybot/index.js")
 var myBot = db.get("botOn");
@@ -19,9 +23,12 @@ console.log("Website starting...");
 app.get("/up/", (req, res) => res.json({ myBot, flyvalle, rambambot, foxy, site: "Up", uptime: new Date(Math.round(process.uptime()) * 1000).toISOString().slice(11, -5) }));
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, '/public/down.html')));
 
+
 try {
   app.listen(port);
-  console.log(`Website on! Port: ${port} Link: www.jackthefine.dev/`);
+  app1.listen(port1);
+  console.log(`Website on! Port: ${port}`);
+  console.log(`L freshies on Port: ${port1}`);
 } catch (error) {
   console.error(error)
 }
