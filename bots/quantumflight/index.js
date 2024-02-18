11676636b510a6fa9fb556b7f379bf557d95e6fd
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2024-02-18 11:09:17 -0500

    Update index.js

--- bots/quantumflight/index.js
+++ bots/quantumflight/index.js
@@ -2,7 +2,7 @@ const trellolink = "https://api.trello.com/1/lists/65d17e08ea7201e9d96f4326/card
 const webhooklink = "https://discord.com/api/webhooks/1208614548266025032/jIb5n3KqQet1Mtd6gNTWczTe6ZvN-7cO6hx1xjBsB_cn0fLUf5sx7sI797poBNggLJI4"
 const editmsg = "1208620228062486591"
 const fetch = require("node-fetch");
-const { EmbedBuilder, WebhookClient } = require("discord.js");
+const { WebhookClient } = require("discord.js");
 
 setInterval(async () => {
 	const data = await (await fetch(trellolink)).json(),
@@ -11,8 +11,9 @@ setInterval(async () => {
 	let content = ``;
 	data.forEach(list => content += list.desc + "\n\n");
   if (content == '') {
-	return
+	content = `There are no current flights at this time, check back later for new flights.
+This message will update every few minutes.`
 	}
-		webhookClient.editMessage(editmsg, { content, username: "Quantum Assistant", avatarURL: 'https://cdn.discordapp.com/avatars/1204908654784946236/feb86cf50354dc944a233ee7359639fb?size=1024', embeds: null});
+		webhookClient.editMessage(editmsg, { content, username: "Quantum Assistant", avatarURL: 'https://cdn.discordapp.com/avatars/1204908654784946236/feb86cf50354dc944a233ee7359639fb?size=1024'});
 
-}, 30000);
+}, 60000);
