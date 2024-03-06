7dec3564ec8dff3aab3ff212741db6ea316c6bd3
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2024-03-05 23:26:39 -0500

    Update static.js

--- static.js
+++ static.js
@@ -43,6 +43,7 @@ app.get("/ranker", (req, res) => {
 app.get("/up/", (req, res) => res.json({ myBot, flyvalle, rambambot, foxy, site: "Up", uptime: new Date(Math.round(process.uptime()) * 1000).toISOString().slice(11, -5) }));
 app.get("/spagpt", (req, res) => res.sendFile(path.join(__dirname, '/public/spagpt.html')));
 app.get("/imgs/roll.gif", (req, res) => res.sendFile(path.join(__dirname, '/public/imgs/roll.gif')));
+app.get("/imgs/moodytoody.png", (req, res) => res.sendFile(path.join(__dirname, '/public/imgs/moodytoody.png')));
 
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, '/public/down.html')));
