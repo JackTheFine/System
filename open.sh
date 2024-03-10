1369c75344c1f82a65fba33272f67161436fcd45
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2024-03-10 13:29:50 -0400

    we

--- open.sh
+++
@@ -1,2 +0,0 @@
-cd /home/jackthefine/Downloads/bot
-node index.js
--- static.js
+++ static.js
@@ -40,7 +40,7 @@ app.get("/ranker", (req, res) => {
     res.json("Ranked!");
 });
 
-app.get("/up/", (req, res) => res.json({ myBot, flyvalle, rambambot, foxy, site: "Up", uptime: new Date(Math.round(process.uptime()) * 1000).toISOString().slice(11, -5) }));
+app.get("/up/", (req, res) => res.json({ site: "Up", uptime: new Date(Math.round(process.uptime()) * 1000).toISOString().slice(11, -5) }));
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, '/public/404.html')));
 
