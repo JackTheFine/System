764a9529c9293d9a68676dc3f23515188f6e1f45
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2024-03-12 18:53:06 -0400

    Update quota.js

--- bots/ccbot/special/quota.js
+++ bots/ccbot/special/quota.js
@@ -7,10 +7,6 @@ module.exports = {
     .setName('quota')
     .setDescription('Checks all non-quota training profiles.')
     .addSubcommand(subcommand =>
-		subcommand.setName("send")
-			.setDescription("Send quota reminder.")
-	)
-    .addSubcommand(subcommand =>
 		subcommand.setName("check")
 			.setDescription("Check all quota.")
 				.addBooleanOption(option =>
@@ -21,31 +17,6 @@ module.exports = {
 	),
   async execute(interaction, client) {
     if (interaction.member.roles.cache.has("1023321861049823344") || interaction.user.id == "710257546908139649") {
-        switch(interaction.options._subcommand) {
-        case "send":
-        var i = 0
-        const trellolink = 'https://api.trello.com/1/boards/4ZYjvFgW/lists?key=4a90d231a30ce6e3ab41c1046088abb5&token=94087062e584167ccfd2215496b99b019771e54af06cc8d4a05835871f055cc1'
-        const data = await (await fetch(trellolink)).json()
-        let longest = data.map(card => card.name.length).sort((a, b) => a - b).at(-1)
-        let message = "```";
-        message += "Name".padEnd(longest) + " | " + "Cards\n"
-        message += "".padEnd(longest, "-") + "--------\n"
-        interaction.reply("Generating message... please wait");
-        for await (const card of data) {
-            const carddata = await (await fetch(`https://api.trello.com/1/lists/${card.id}/cards?key=4a90d231a30ce6e3ab41c1046088abb5&token=94087062e584167ccfd2215496b99b019771e54af06cc8d4a05835871f055cc1`)).json()
-            if (carddata.length >= 3) continue;
-            message += `${card.name.padEnd(longest)} | ${carddata.length}\n`;
-            const member = interaction.guild.members.cache.find(member => member.user.tag == card.name);
-
-            (await 
-              console.log(member.id)
-            );
-        }
-
-        message += "```";
-        interaction.editReply({ content: `Sending message to the following people:\n${message}` })
-        break;
-        case "check": 
         var i = 0
         const trellolink1 = 'https://api.trello.com/1/boards/4ZYjvFgW/lists?key=4a90d231a30ce6e3ab41c1046088abb5&token=94087062e584167ccfd2215496b99b019771e54af06cc8d4a05835871f055cc1'
         const data2 = await (await fetch(trellolink1)).json()
@@ -61,8 +32,6 @@ module.exports = {
         }
         message2 += "```";
         interaction.editReply({ content: message2 })
-        break;
-    } 
     } else interaction.reply({ content: "Invalid Permissions", ephemeral: true})
   }
 }
